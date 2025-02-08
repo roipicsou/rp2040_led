3ed99e534582110ab929c588815b49b779fa9e1c
roipicsou <pl613330@gmail.com>
2025-02-08 13:07:15 +0100

    2

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-# Remplacez par le port série de votre Smart Pi One (ex: "/dev/ttyAMA0" sous Linux)
-PORT = "/dev/ttyAMA0"
+# Remplacez par le port série de votre carte (ex: "COM3" sous Windows, "/dev/ttyUSB0" sous Linux/macOS)
+PORT = "COM5"
 BAUDRATE = 9600
 load_dotenv()
-
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 try:
@@ -31,26 +30,26 @@ def send_command(command):
 
 @bot.event
 async def on_ready():
-    print("Le bot est en ligne.")
-    try:
+    print("le bot est un route")
+    try :
         synced = await bot.tree.sync()
-        print(f"Commandes synchronisées : {len(synced)}")
-    except Exception as e:
+        print(f"Comande sincroniée : {len(synced)}")
+    except Exception as e :
         print(e)
 
-@bot.tree.command(name="led_on", description="Allume les LEDs")
-async def led_on(interaction: discord.Interaction):
+@bot.tree.command(name="led_on", description="alumme les led")
+async def led_on(interaction : discord.Interaction) :
     send_command("ON")
-    await interaction.response.send_message("LED allumées")
+    await interaction.response.send_message("led on")
 
-@bot.tree.command(name="led_off", description="Éteint les LEDs")
-async def led_off(interaction: discord.Interaction):
+@bot.tree.command(name="led_off", description="eteind les led")
+async def led_off(interaction : discord.Interaction) :
     send_command("OFF")
-    await interaction.response.send_message("LED éteintes")
+    await interaction.response.send_message("led off")
 
-@bot.tree.command(name="c_lum", description="Changer l'intensité des LEDs")
-async def c_lum(interaction: discord.Interaction, lm: int):
+@bot.tree.command(name="c", description="changer l'intensitéer des led")
+async def c_lum(interaction : discord.Interaction, lm: int) :
     send_command(f"C_LUM,{lm}")
-    await interaction.response.send_message(f"L'intensité des LEDs est changée à {lm}")
+    await interaction.response.send_message(f"la luminosiée est changer a {lm}")
 
 bot.run(os.getenv('DISCORD'))
